59d09d422128b335248b2a30d502b798046df2a0
amosorio <am.osorio@uniandes.edu.co>
2016-03-27 11:53:45 -0500

    Actualizacion de servicio rest de registro de usuarios, para funcionamiento adecuado

--- logica/src/main/java/servicios/rest/RestRegistro.java
+++ logica/src/main/java/servicios/rest/RestRegistro.java
@@ -1,9 +1,12 @@
 package servicios.rest;
 
+import java.util.Date;
 import java.util.concurrent.ThreadLocalRandom;
 
 import javax.persistence.EntityManager;
+import javax.persistence.NoResultException;
 
+import org.hibernate.metamodel.source.hbm.Helper.ValueSourcesAdapter;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -12,63 +15,64 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import fabricas.entidades.Perfiles;
+import fabricas.entidades.Servicio;
 import fabricas.entidades.Usuario;
 
 @RestController
 @RequestMapping("/registro")
 public class RestRegistro {
-	
-	
-	@RequestMapping(value = "/nuevo/{filtros}", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
-	public ResponseEntity <String> getAlojamientoByFilter(@PathVariable String filtros) {
+
+
+	@RequestMapping(value = "/{filtrosBasicos}/{filtrosAvanzados}/{filtrosAvanzados2}", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+	public ResponseEntity <String> getAlojamientoByFilter(@PathVariable String filtrosBasicos,@PathVariable String filtrosAvanzados,@PathVariable String filtrosAvanzados2) {
 
 		EntityManager em = PersistenceManager.INSTANCE.getEntityManager();
 		em.getTransaction().begin();
-
-		int id = ThreadLocalRandom.current().nextInt(15, 150 + 1);;
 		String nombre="";
 		String apellido="";
 		String email="";
 		String direccion="";
 		String telefono="";
-		String fecha="2016-03-20 00:00:00";
-		int login = 1;
+		Date fecha= new Date();
 		String password="";
-		int activo = 1;
-		String perfil = "";
-		String response= "Error, revise su correo";
+		String perfil="";
+
+		String[]values = filtrosBasicos.split(",");
+		nombre=values[0];
+		apellido=values[1];
+		email=values[2];
 		
-		for (String filtro : filtros.split(",")) {
-			String[]values = filtro.split("=");
-			if(values.length==2){
-				if (values[0].equals("nombre")){
-					nombre=values[1];
-				}else if (values[0].equals("apellido")){
-						apellido=values[1];
-				}else if(values[0].equals("email")){
-					email=values[1];
-				}else if(values[0].equals("password")){
-					password=values[1];
-				}else if(values[0].equals("direccion")){
-					direccion=values[1];
-				}else if(values[0].equals("telefono")){
-					telefono=values[1];
-				}else if(values[0].equals("tipoUsuario")){
-					perfil=values[1];
-				}
-			}
-		}
+		values = filtrosAvanzados.split(",");
+		password = values[0];
+		direccion=values[1];
 		
-		String query = "Select * from Usuarios where email = "+email;
-		if((Usuario) em.createQuery(query).getSingleResult() != null){
-//			query="insert into usuarios values (8,"Diego","Farfan","d.farfan10","direccion",12343,"2012-06-20 00:00:00",2,"diego",1,2)";
-			query = "insert into usuarios values ("+id+","+nombre+","+apellido+","+email+","+direccion+","+telefono+","+fecha+","+login+","+
-					password+","+activo+","+perfil+");";
-			
-			em.createQuery(query);
-			response="Se ha registrado con éxito";				
+		values = filtrosAvanzados2.split(",");
+		telefono=values[0];
+		perfil=values[1];
+	
+
+		try{
+			Usuario usuario = (Usuario) em.createNamedQuery("Usuario.findByEmail").setParameter("email", email).getSingleResult();
+		}catch(NoResultException e){
+			Usuario usuario = new Usuario();
+			usuario.setNombre(nombre);
+			usuario.setApellido(apellido);
+			usuario.setEmail(email);
+			usuario.setPassword(password);
+			usuario.setDireccion(direccion);
+			usuario.setTelefono(telefono);
+			usuario.setFechaCreacion(fecha);
+			usuario.setLogin("1");
+			usuario.setActivo(true);
+			Perfiles perfiles = new Perfiles();
+			perfiles.setIdperfil(new Integer(perfil));
+			usuario.setPerfil(perfiles);
+			em.persist(usuario);
+			em.getTransaction().commit();
+			return new ResponseEntity <String> ("Se ha registrado con éxito", HttpStatus.OK);
 		}
 
-		return new ResponseEntity <String> (response, HttpStatus.OK);
+		return new ResponseEntity <String> ("Ya hay un usuario registrado con ese correo!", HttpStatus.OK);		
 	}
 }
